e9ec363953ef826cac89eef5f11f13b908691e81
Soloman Antony <solomanantony123@gmail.com>
2025-08-27 14:02:44 +0530

    Add files via upload

---
+++ age_pgm.py
@@ -0,0 +1,9 @@
+age=int(input("enter your age: "))
+if age<=0:
+    print("lol you are not born yet")
+elif age>=20:
+    print("you are an adult")
+elif age<=12:
+    print("you are a child")
+else:
+    print("you are a teenager")
